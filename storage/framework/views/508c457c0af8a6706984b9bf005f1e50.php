7480349022fe14dee071046ead49f150612b0ebe
Ananda Madani <114144114+AnandaMadani29@users.noreply.github.com>
2024-11-08 01:07:04 +0700

    Add files via upload

---
+++ storage/framework/views/508c457c0af8a6706984b9bf005f1e50.php
@@ -0,0 +1,11 @@
+
+
+<?php $__env->startSection('content'); ?>
+<h1>Welcome to EduFun</h1>
+<?php $__currentLoopData = $posts; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $post): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+    <h2><?php echo e($post->title); ?></h2>
+    <p><?php echo e($post->content); ?></p>
+<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\laragon\www\Quiz1-WebProg\resources\views/home.blade.php ENDPATH**/ ?>
